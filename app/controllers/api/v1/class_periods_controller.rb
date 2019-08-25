b8f60e3d6435e21ceb8c10eaa332e8b950d8c6ca
Lauren Shultz <lshultz2020@gmail.com>
2019-08-24 21:47:03 -0500

    added home page

---
+++ app/controllers/api/v1/class_periods_controller.rb
@@ -0,0 +1,52 @@
+class Api::V1::ClassPeriodsController < ApplicationController
+  skip_before_action :verify_authenticity_token
+  before_action :set_class_period, only: [:show, :update, :destroy]
+
+  # GET /communities
+  def index
+    @class_period = ClassPeriod.all
+
+    render json: @class_period
+  end
+
+  # GET /communities/1
+  def show
+    render json: @class_period
+  end
+
+  # POST /communities
+  def create
+    @class_period = ClassPeriod.new(class_period_params)
+
+    if @class_period.save
+      render json: @class_period, status: :created, location: api_v1_class_period_url(@class_period)
+    else
+      render json: @class_period.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /communities/1
+  def update
+    if @class_period.update(class_period_params)
+      render json: @class_period
+    else
+      render json: @class_period.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /communities/1
+  def destroy
+    @class_period.destroy
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_class_period
+      @class_period = ClassPeriod.find(params[:id])
+    end
+
+    # Only allow a trusted parameter "white list" through.
+    def class_period_params
+        params.permit(:date, :week_day, :attendance_count)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,10 @@ Rails.application.routes.draw do
   resources :schools
   resources :class_periods
   resources :staff
+  namespace :api do
+    namespace :v1 do
+      resources :class_periods
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html   https://ck-internal.herokuapp.com/
 end
